668b6e4ca1b14cecb32bd1bd320cb6afdf0e0ac0
Rafal Brzoska <rbrzoska@future-processing.com>
2020-06-18 13:21:38 +0200

    products observables

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from 'src/app/models/Product';
 import { ProductsService } from '../products.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'fp-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
 
+  subscription: Subscription;
   showDescriptions = false;
-  products: Product[];
+  products;
+  products$: Observable<Product[]>;
   constructor(private productService: ProductsService) {
-    this.products = this.productService.getAllProducts()
+    this.subscription = this.productService.products$.subscribe( p => console.log(p))
+    this.products$ = this.productService.products$;
+    this.productService.refreshProducts();
    }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/Product';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private _products: Product[] = [
-    { id: '1', name: 'prod1', description: 'prod descr 1', price: 1.99, category: 'cat1' },
-    { id: '2', name: 'prod2', description: 'prod descr 2', price: 2.99, category: 'cat2' }
-  ]
-  constructor() { }
+  private _productsSubj = new BehaviorSubject<Product[]>(null);
+  products$ = this._productsSubj.asObservable();
 
-  getAllProducts() {
-    return this._products;
+  constructor(private http: HttpClient) { }
+
+  refreshProducts(queryFilter = '') {
+    return this.http.get<Product[]>('http://localhost:3000/products', {
+      params: {
+        q: queryFilter
+      }
+    })
+    .subscribe(p => this._productsSubj.next(p));
   }
 }
